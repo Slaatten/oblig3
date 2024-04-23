1b7139b661618d533e95ec20af194c0f1b20a972
Slaatten <stsla0672@oslomet.no>
2024-04-23 14:07:37 +0200

    Lagde ferdig HTMl

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,15 +1,10 @@
-CREATE TABLE Film (
-    film VARCHAR(20) NOT NULL,
-    PRIMARY KEY (film)
-);
-
 CREATE TABLE Billett (
     id smallint NOT NULL AUTO_INCREMENT,
+    film VARCHAR(30),
     antall INTEGER NOT NULL,
     fornavn VARCHAR(30),
     etternavn VARCHAR(30),
     telefon VARCHAR(8),
     epost VARCHAR(30),
     PRIMARY KEY (id),
-    FOREIGN KEY (film) references Film(film)
 );
